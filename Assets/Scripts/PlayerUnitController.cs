3bd88e4cfa4dc535c8bb2301717b163bd4f64d2c
edmondmw <edmondmwang@gmail.com>
2017-10-11 02:47:56 -0700

    Created a moving unit that can shoot projectiles

---
+++ Assets/Scripts/PlayerUnitController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUnitController : MonoBehaviour {
+    // Should have an array of player controlled units
+    public GameObject[] units;
+    public GameObject snowball;
+
+    Camera cam;
+    UnitMotor motor;
+    int selectedIndex = 0;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        cam = Camera.main;
+        motor = units[selectedIndex].GetComponent<UnitMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveHandler();
+        AttackHandler();     
+	}
+
+    void MoveHandler()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.point);
+                motor.Move(hit.point);
+            }
+        }
+    }
+
+    void AttackHandler()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject aSnowball = Instantiate(snowball, units[selectedIndex].transform.position, units[selectedIndex].transform.rotation);
+            aSnowball.GetComponent<Rigidbody>().AddForce(units[selectedIndex].transform.forward * 1000f);
+        }
+    }
+}
---
+++ Assets/Scripts/UnitMotor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class UnitMotor : MonoBehaviour {
+
+    NavMeshAgent nav;
+    Vector3 destination;
+    bool isMoving = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        nav = GetComponent<NavMeshAgent>();
+	}
+
+    void Update()
+    {
+        // When we reach destination, face forward
+        // TODO: Find a better way to determine when finished moving, This won't work for out of bounds clicks
+        if (transform.position.x == destination.x && transform.position.z == destination.z)
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+
+    }
+
+    public void Move(Vector3 point)
+    {
+        isMoving = true;
+        nav.SetDestination(point);
+        destination = point;
+    }
+}
